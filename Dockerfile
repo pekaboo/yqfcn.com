71a748415aff3aa76426b51382028066c545fbcb
学长的猫 <me.wangjun@qq.com>
2023-09-13 12:04:23 +0300

    fix

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM harbor.j-net.cn/wms/nginx
+RUN mkdir /app
+# COPY  /dist /app
+COPY  * /app
+COPY /dist/nginx.conf /etc/nginx/nginx.conf
---
+++ setup.sh
@@ -0,0 +1,25 @@
+# git clone https://github.com/pekaboo/yqfcn.com.git
+APP_NAME=html01
+APP_TAG=latest
+# HARBOR_PREFIX=harbor.j-net.cn/jnet-mall
+HARBOR_PREFIX=yqfcn
+
+docker ps -a
+docker image rm -f ${HARBOR_PREFIX}/${APP_NAME}:${APP_TAG}
+# docker pull ${HARBOR_PREFIX}/${APP_NAME}:${APP_TAG}
+docker build ${HARBOR_PREFIX}/${APP_NAME}:${APP_TAG}
+docker container rm -f ${APP_NAME}
+
+docker run \
+-it -d \
+--restart=always \
+--name jnet-mall-${APP_NAME} \
+--log-opt max-size=10m \
+--log-opt max-file=5 \
+-e TZ=Asia/Shanghai \
+-e SPRING_PROFILES_ACTIVE=prod \
+-e SPRING_OUTPUT_ANSI_ENABLED=NEVER \
+-p 7089:3000 \
+${HARBOR_PREFIX}/${APP_NAME}:${APP_TAG}
+
+docker ps -l --no-trunc
